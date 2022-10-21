d666ee2b8a42873f0abb4cefbe3f876b1db9521e
prv-bperez <prv_bperez@ath.com.co>
2022-10-21 14:56:23 -0700

    Se ajustan servicios denominacion cantidad

--- src/main/java/com/ath/adminefectivo/entities/TdvDenominCantidad.java
+++ src/main/java/com/ath/adminefectivo/entities/TdvDenominCantidad.java
@@ -6,6 +6,8 @@ import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -36,8 +38,9 @@ public class TdvDenominCantidad {
 	
 	
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id_tdv_den_cant")
-	private int idTdvDenCant;
+	private Integer idTdvDenCant;
 
 	@ManyToOne
 	@JoinColumn(name = "CODIGO_TDV", nullable = false)
