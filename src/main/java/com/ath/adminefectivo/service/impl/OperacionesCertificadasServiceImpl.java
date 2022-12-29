76d4a88d9516df41b7020baa17fa939a516156ae
Duvan Steven Naranjo Martinez <prv_dnaranjo@ath.com.co>
2022-12-29 18:01:46 -0500

    ajustes don Rafael

--- src/main/java/com/ath/adminefectivo/service/impl/OperacionesCertificadasServiceImpl.java
+++ src/main/java/com/ath/adminefectivo/service/impl/OperacionesCertificadasServiceImpl.java
@@ -663,26 +663,22 @@ public class OperacionesCertificadasServiceImpl implements IOperacionesCertifica
 	 * @author cesar.castano
 	 */
 	private Double asignarValorTotal(String[] fila, Integer numeroInicia, Integer longitud) {
-		Double valorAcumulado = 0.0;
-		boolean esPesos = true;
-		int menorDenominacionCop = parametroService.valorParametroEntero(Constantes.MIN_DENOM_COP);
-		
-		
-		
-		for (var i = numeroInicia; i < longitud; i = i + 2) {
-			if (numeroInicia.compareTo(Constantes.INICIA_DENOMINACION_BRINKS) == 0 ) {
-				// determinar si la moneda es diferente a COP
-				dominioService.valorTextoDominio(Constantes.DOMINIO_DIVISAS, Dominios.PESOS);
-				esPesos = fila[i + 1].trim().equals(dominioService.valorTextoDominio(Constantes.DOMINIO_DIVISAS, Dominios.PESOS));
-			}
-			Double denonimacion = Double.parseDouble(fila[i].trim());
-			if (denonimacion >= menorDenominacionCop || !esPesos ) {
-				valorAcumulado = valorAcumulado + (denonimacion * Double.parseDouble(fila[i + 1].trim()));
-			}
-
-		}
-		return valorAcumulado;
-	}
+        Double valorAcumulado = 0.0;
+        Double denonimacion = 0.0;
+        boolean esPesos = true;
+        int menorDenominacionCop = parametroService.valorParametroEntero(Constantes.MIN_DENOM_COP);
+        if (numeroInicia.compareTo(Constantes.INICIA_DENOMINACION_BRINKS) == 0 ) {
+            // determinar si la moneda es diferente a COP
+            esPesos = fila[4].trim().equals(dominioService.valorTextoDominio(Constantes.DOMINIO_DIVISAS, Dominios.PESOS));
+        }
+        for (var i = numeroInicia; i < longitud; i = i + 2) {
+            denonimacion = Double.parseDouble(fila[i].trim());
+            if (denonimacion >= menorDenominacionCop || !esPesos ) {
+                valorAcumulado = valorAcumulado + (denonimacion * Double.parseDouble(fila[i + 1].trim()));
+            }
+        }
+        return valorAcumulado;
+    }
 
 	/**
 	 * Metodo encargado de guardar los valores sobrantes y faltantes en una lista
