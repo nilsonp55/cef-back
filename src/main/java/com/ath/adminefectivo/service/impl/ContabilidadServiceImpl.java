5401dc44aa57e2430347df9eb3160dc9127ea49f
NILSON PARRA MARTINEZ <prv_nparra@ath.com.co>
2023-03-21 09:48:46 -0500

    Fecha proceso cierre (#179)
    * establecer fecha finalizacion en proceso de LogProcesoDiario
    
    * cambia nombre de esquema de base de datos, no se usa public
    
    * se agrega parametro para nombre de schema postgresql
    
    * config actuator
    
    * se agrega configuracion de version y endpoint de info
    
    * cambia version jdk para construir app jar
    
    * Se ajsuta proceso de generacion de contabilidad retiro cambio.

--- src/main/java/com/ath/adminefectivo/service/impl/ContabilidadServiceImpl.java
+++ src/main/java/com/ath/adminefectivo/service/impl/ContabilidadServiceImpl.java
@@ -1,23 +1,15 @@
 package com.ath.adminefectivo.service.impl;
 
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 import java.util.Objects;
 
-import javax.transaction.Transactional;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.ath.adminefectivo.constantes.Constantes;
 import com.ath.adminefectivo.constantes.Dominios;
-import com.ath.adminefectivo.delegate.IContabilidadDelegate;
-import com.ath.adminefectivo.dto.ArchivosCargadosDTO;
 import com.ath.adminefectivo.dto.BancosDTO;
 import com.ath.adminefectivo.dto.CiudadesDTO;
 import com.ath.adminefectivo.dto.ErroresContablesDTO;
@@ -30,23 +22,8 @@ import com.ath.adminefectivo.dto.TransaccionesInternasDTO;
 import com.ath.adminefectivo.dto.TransportadorasDTO;
 import com.ath.adminefectivo.dto.compuestos.ContabilidadDTO;
 import com.ath.adminefectivo.dto.compuestos.ConteoContabilidadDTO;
-import com.ath.adminefectivo.dto.compuestos.ErroresCamposDTO;
 import com.ath.adminefectivo.dto.compuestos.OperacionIntradiaDTO;
-import com.ath.adminefectivo.dto.compuestos.TransaccionInternaIntradiaDTO;
-import com.ath.adminefectivo.dto.compuestos.ValidacionArchivoDTO;
-import com.ath.adminefectivo.dto.compuestos.ValidacionLineasDTO;
-import com.ath.adminefectivo.dto.response.ApiResponseCode;
-import com.ath.adminefectivo.entities.ArchivosCargados;
-import com.ath.adminefectivo.entities.FallasArchivo;
-import com.ath.adminefectivo.entities.FallasRegistro;
 import com.ath.adminefectivo.entities.Puntos;
-import com.ath.adminefectivo.entities.RegistrosCargados;
-import com.ath.adminefectivo.entities.TransaccionesInternas;
-import com.ath.adminefectivo.entities.id.FallasRegistroPK;
-import com.ath.adminefectivo.entities.id.RegistrosCargadosPK;
-import com.ath.adminefectivo.exception.NegocioException;
-import com.ath.adminefectivo.repositories.ArchivosCargadosRepository;
-import com.ath.adminefectivo.service.IArchivosCargadosService;
 import com.ath.adminefectivo.service.IBancosService;
 import com.ath.adminefectivo.service.ICiudadesService;
 import com.ath.adminefectivo.service.IContabilidadService;
@@ -58,8 +35,6 @@ import com.ath.adminefectivo.service.IPuntosService;
 import com.ath.adminefectivo.service.ITransaccionesContablesService;
 import com.ath.adminefectivo.service.ITransaccionesInternasService;
 import com.ath.adminefectivo.service.ITransportadorasService;
-import com.fasterxml.jackson.databind.deser.impl.CreatorCandidate.Param;
-import com.querydsl.core.types.Predicate;
 
 @Service
 public class ContabilidadServiceImpl implements IContabilidadService {
@@ -192,7 +167,7 @@ public class ContabilidadServiceImpl implements IContabilidadService {
 	 */
 	private void procesarTransaccionesInternas(String tipoContabilidad, TransaccionesInternasDTO transaccionInterna) {
 		// TODO Auto-generated method stub
-
+		
 	}
 
 	/**
@@ -308,8 +283,7 @@ public class ContabilidadServiceImpl implements IContabilidadService {
 	 */
 	private int procesarContabilidadConsignacion(String tipoProceso, OperacionesProgramadasDTO operacionProgramada) {
 
-		TransaccionesInternasDTO transaccionInternaDTO = generarTransaccionInterna(tipoProceso, 10, operacionProgramada,
-				operacionProgramada.getCodigoFondoTDV());
+		generarTransaccionInterna(tipoProceso, 10, operacionProgramada, operacionProgramada.getCodigoFondoTDV());
 
 		return consecutivoDia;
 	}
@@ -320,7 +294,7 @@ public class ContabilidadServiceImpl implements IContabilidadService {
 	 * @return
 	 */
 	private int procesarContabilidadRetiro(String tipoProceso, OperacionesProgramadasDTO operacionProgramada) {
-		TransaccionesInternasDTO transaccionInternaDTO = generarTransaccionInterna(tipoProceso, 20, operacionProgramada, operacionProgramada.getCodigoFondoTDV());
+		generarTransaccionInterna(tipoProceso, 20, operacionProgramada, operacionProgramada.getCodigoFondoTDV());
 		
 		if(!Objects.isNull(operacionProgramada.getComisionBR()) && operacionProgramada.getComisionBR() > 0) {
 			TransaccionesInternasDTO transaccionInternaDTOComision = generarTransaccionInterna(tipoProceso, 21, operacionProgramada, operacionProgramada.getCodigoFondoTDV());
@@ -356,7 +330,7 @@ public class ContabilidadServiceImpl implements IContabilidadService {
 	 * @return
 	 */
 	private int procesarContabilidadVenta(String tipoProceso, OperacionesProgramadasDTO operacionProgramada) {
-		TransaccionesInternasDTO transaccionInternaDTOVenta = null;
+		
 		PuntosDTO puntoOrigen = PuntosDTO.CONVERTER_DTO
 				.apply(puntosService.getPuntoById(operacionProgramada.getCodigoPuntoOrigen()));
 		PuntosDTO puntoDestino = PuntosDTO.CONVERTER_DTO
@@ -509,34 +483,10 @@ public class ContabilidadServiceImpl implements IContabilidadService {
 	 * @return
 	 */
 	private int procesarContabilidadRetiroCambio(String tipoProceso, OperacionesProgramadasDTO operacionProgramada) {
-		TransaccionesInternasDTO transaccionInternaDTO = generarTransaccionInterna(tipoProceso, 20, operacionProgramada, operacionProgramada.getCodigoFondoTDV());
 		
-		if(!Objects.isNull(operacionProgramada.getComisionBR()) && operacionProgramada.getComisionBR() > 0) {
-			TransaccionesInternasDTO transaccionInternaDTOComision = generarTransaccionInterna(tipoProceso, 21, operacionProgramada, operacionProgramada.getCodigoFondoTDV());
-			transaccionInternaDTOComision.setValor(operacionProgramada.getComisionBR().longValue());
-			transaccionInternaDTOComision.setCodigoComision(Integer.valueOf(Dominios.COMISION_1));
-			transaccionesInternasService.saveTransaccionesInternasById(transaccionInternaDTOComision);
-			
-			long valorImpuesto = 0;
-			if (isCiudadCobroIVA(operacionProgramada.getCodigoFondoTDV()) ) {	
-				TransaccionesInternasDTO transaccionInternaDTOImpuesto = generarTransaccionInterna(tipoProceso, 22, operacionProgramada, operacionProgramada.getCodigoFondoTDV());
-				valorImpuesto = this.calcularValorConImpuesto(operacionProgramada.getComisionBR(), Dominios.IMPUESTO_IVA);
-				transaccionInternaDTOImpuesto.setValor(valorImpuesto);
-				transaccionInternaDTOImpuesto.setCodigoComision(Integer.valueOf(Dominios.COMISION_1));
-				transaccionInternaDTOImpuesto.setTipoImpuesto(Integer.valueOf(Dominios.IMPUESTO_IVA));
-	
-				transaccionesInternasService.saveTransaccionesInternasById(transaccionInternaDTOImpuesto);
-			}
-			
-			
-			TransaccionesInternasDTO transaccionInternaDTOMedioPago = generarTransaccionInterna(tipoProceso, 23, operacionProgramada, operacionProgramada.getCodigoFondoTDV());
-			long valorD = operacionProgramada.getComisionBR() + valorImpuesto;
-			transaccionInternaDTOMedioPago.setValor(valorD);
-			transaccionInternaDTOMedioPago.setMedioPago(Dominios.MEDIOS_PAGO_DESCUENTO);	
-			transaccionesInternasService.saveTransaccionesInternasById(transaccionInternaDTOMedioPago);
-
-		}
-
+		TransaccionesInternasDTO transaccionInternaDTO = generarTransaccionInterna(tipoProceso, 20, operacionProgramada, operacionProgramada.getCodigoFondoTDV());
+		transaccionInternaDTO.setEsCambio(true);
+		transaccionesInternasService.saveTransaccionesInternasById(transaccionInternaDTO);
 		return consecutivoDia;
 	}
 
