a5e6447901fb53697683cd77548d6aac81d8f58b
Nilson Parra <prv_nparra@ath.com.co>
2024-03-17 12:09:29 -0500

    unit test for repository layer

---
+++ src/test/java/com/ath/adminefectivo/repositories/IOperacionesCertificadasRepositoryTest.java
@@ -0,0 +1,103 @@
+package com.ath.adminefectivo.repositories;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+@Disabled
+class IOperacionesCertificadasRepositoryTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void testFindByEstadoConciliacionNotAndConciliable() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCountByEstadoConciliacionAndFechaEjecucionBetween() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCountByEstadoConciliacionAndFechaEjecucionBetweenAndConciliable() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFindByCodigoFondoTDVAndEstadoConciliacion() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFindByCodigoServicioTdv() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFindByEstadoConciliacion() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFindByCodigoPuntoOrigenAndCodigoServicioTdvAndEntradaSalidaAndFechaEjecucion() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFindByCodigoFondoTDVAndCodigoPuntoOrigenAndCodigoPuntoDestinoAndCodigoServicioTdvAndEntradaSalidaAndFechaEjecucionAndCodigoPropioTDVAndIdArchivoCargado() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testConciliacionAutomatica() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFindByCodigoPuntoDestinoAndEntradaSalidaAndFechaEjecucion() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFindByCodigoPuntoOrigenAndEntradaSalidaAndFechaEjecucion() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testValidarnoconciliables() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testProcesarArchivosAlcance() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSave() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFindById() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFindAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDelete() {
+		fail("Not yet implemented");
+	}
+
+}
