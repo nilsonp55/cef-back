23cd2df37db928571adebf901aea1f73533d255b
Nilson Parra <prv_nparra@ath.com.co>
2024-03-17 12:09:29 -0500

    unit test for repository layer

---
+++ src/test/java/com/ath/adminefectivo/repositories/ICiudadesRepositoryTest.java
@@ -0,0 +1,63 @@
+package com.ath.adminefectivo.repositories;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+@Disabled
+class ICiudadesRepositoryTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void testFindByNombreCiudad() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFindBycodigoDANE() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFindByNombreCiudadFiserv() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFindByCodigoBrinks() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFindAllByOrderByNombreCiudadAsc() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSave() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFindById() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFindAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDelete() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/com/ath/adminefectivo/repositories/IClientesCorporativosRepositoryTest.java
@@ -0,0 +1,48 @@
+package com.ath.adminefectivo.repositories;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+@Disabled
+class IClientesCorporativosRepositoryTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void testFindByCodigoBancoAvalAndIdentificacion() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFindByCodigoCliente() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSave() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFindById() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFindAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDelete() {
+		fail("Not yet implemented");
+	}
+
+}
