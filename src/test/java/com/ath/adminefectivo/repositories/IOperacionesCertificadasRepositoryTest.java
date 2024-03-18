136103a110f507b83ca9ed24aaa85eae189ecc45
Nilson Parra <prv_nparra@ath.com.co>
2024-03-18 06:45:33 -0500

    unit test for repository layer

--- src/test/java/com/ath/adminefectivo/repositories/IOperacionesCertificadasRepositoryTest.java
+++ src/test/java/com/ath/adminefectivo/repositories/IOperacionesCertificadasRepositoryTest.java
@@ -5,14 +5,58 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import com.ath.adminefectivo.entities.OperacionesCertificadas;
 
 @DataJpaTest
 @Disabled
 class IOperacionesCertificadasRepositoryTest {
+	
+	@Autowired
+	private IOperacionesCertificadasRepository operacionesCertificadasRepository;
+	
+	private OperacionesCertificadas operacionesCertificadas;
 
 	@BeforeEach
 	void setUp() throws Exception {
+		
+		operacionesCertificadas = OperacionesCertificadas.builder()
+				.bancoAVAL(null)
+				.codigoFondoTDV(null)
+				.codigoOperacion(null)
+				.codigoPropioTDV(null)
+				.codigoPuntoCodigotdv(null)
+				.codigoPuntoDestino(null)
+				.codigoPuntoOrigen(null)
+				.codigoServicioTdv(null)
+				.conciliable(null)
+				.consecutivoRegistro(null)
+				.descripcionPuntoCodigotdv(null)
+				.entradaSalida(null)
+				.estadoConciliacion(null)
+				.fallidaOficina(null)
+				.fechaEjecucion(null)
+				.idArchivoCargado(null)
+				.idCertificacion(null)
+				.moneda(null)
+				.nombreFondoTDV(null)
+				.nombrePuntoDestino(null)
+				.nombrePuntoOrigen(null)
+				.oficina(null)
+				.tdv(null)
+				.tipoOperacion(null)
+				.tipoPuntoDestino(null)
+				.tipoServicio(null)
+				.valorFaltante(null)
+				.valorSobrante(null)
+				.valorTotal(null)
+				.fechaCreacion(null)
+				.fechaModificacion(null)
+				.usuarioCreacion(null)
+				.usuarioModificacion(null)
+				.build();
 	}
 
 	@Test
---
+++ src/test/java/com/ath/adminefectivo/repositories/IOperacionesProgramadasRepositoryTest.java
@@ -0,0 +1,143 @@
+package com.ath.adminefectivo.repositories;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@Disabled
+@DataJpaTest
+class IOperacionesProgramadasRepositoryTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void testFindAllPredicatePageable() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFindByEstadoConciliacion() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFindByFechaOrigen() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFindByEstadoConciliacionNot() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testConciliacionManual() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCountByEstadoConciliacionAndFechaOrigenBetween() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFindByCodigoFondoTDVAndEstadoConciliacion() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFindByIdArchivoCargado() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFindByTipoOperacionAndFechaProgramacionBetween() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFindByTipoOperacionAndFechaOrigenBetweenAndEsCambio() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFindByTipoOperacionAndFechaOrigenBetweenAndEsCambioAndEstadoConciliacion() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testConciliacionAutomatica() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFindByIdServicioAndIdArchivoCargado() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testConsultaOperacionesIntradiaSalida() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testConsultaOperacionesIntradiaEntrada() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testObtenerConErroresContables() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testReabrirCertificaciones() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testReabrirDefinitiva() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testReabrirPreliminar() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testReabrirConciliaciones() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFindByIdServicioAndIdArchivoCargadoAndEntradaSalida() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSave() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFindById() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFindAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDelete() {
+		fail("Not yet implemented");
+	}
+
+}
