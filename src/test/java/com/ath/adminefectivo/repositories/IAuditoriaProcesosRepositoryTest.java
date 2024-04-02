764cff63f10b9cad5962ae7a9db0532b0b084e0d
Nilson Parra <prv_nparra@ath.com.co>
2024-04-02 10:24:59 -0500

    unit test for repository layer

--- src/test/java/com/ath/adminefectivo/repositories/IAuditoriaProcesosRepositoryTest.java
+++ src/test/java/com/ath/adminefectivo/repositories/IAuditoriaProcesosRepositoryTest.java
@@ -1,38 +1,78 @@
 package com.ath.adminefectivo.repositories;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.instancio.Instancio;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import com.ath.adminefectivo.entities.AuditoriaProcesos;
+
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
 @DataJpaTest
-@Disabled
 class IAuditoriaProcesosRepositoryTest {
+
+	@Autowired
+	private IAuditoriaProcesosRepository auditoriaProcesosRepository;
+	private AuditoriaProcesos auditoriaProcesos;
+	private List<AuditoriaProcesos> listOfAuditoriaProcesos;
+	private AuditoriaProcesos auditoriaProcesosSearch;
 
 	@BeforeEach
 	void setUp() throws Exception {
+		auditoriaProcesos = Instancio.of(AuditoriaProcesos.class).create();
+		List<AuditoriaProcesos> listSave = Instancio.ofList(AuditoriaProcesos.class).size(11).create();
+		listOfAuditoriaProcesos = auditoriaProcesosRepository.saveAll(listSave);
+		auditoriaProcesosSearch = listOfAuditoriaProcesos.get(0);
+
+		log.info("setup - id: {}", auditoriaProcesos.getId());
 	}
 
 	@Test
 	void testIAuditoriaProcesosSave() {
-		fail("Not yet implemented");
+		AuditoriaProcesos auditoriaProcesosSaved = auditoriaProcesosRepository.save(auditoriaProcesos);
+
+		assertThat(auditoriaProcesosSaved).isNotNull();
+		assertThat(auditoriaProcesosSaved.getId()).isEqualTo(auditoriaProcesos.getId());
+		log.info("testIAuditoriaProcesosSave - id: {}", auditoriaProcesosSaved.getId());
 	}
 
 	@Test
 	void testIAuditoriaProcesosFindById() {
-		fail("Not yet implemented");
+		Optional<AuditoriaProcesos> auditoriaProcesosFind = auditoriaProcesosRepository
+				.findById(auditoriaProcesosSearch.getId());
+
+		assertThat(auditoriaProcesosFind).isNotEmpty();
+		auditoriaProcesosFind.ifPresent(auditoriaProcesos -> {
+			assertThat(auditoriaProcesos.getId()).isEqualTo(auditoriaProcesosSearch.getId());
+			assertThat(auditoriaProcesos.getFechaCreacion()).isEqualTo(auditoriaProcesosSearch.getFechaCreacion());
+		});
+		log.info("testIAuditoriaProcesosFindById - id: {}", auditoriaProcesosSearch.getId());
 	}
 
 	@Test
 	void testIAuditoriaProcesosFindAll() {
-		fail("Not yet implemented");
+		List<AuditoriaProcesos> auditoriaProcesosFind = auditoriaProcesosRepository.findAll();
+
+		assertThat(auditoriaProcesosFind).isNotEmpty().hasSize(listOfAuditoriaProcesos.size());
+		log.info("testIAuditoriaProcesosFindAll - size: {}", auditoriaProcesosFind.size());
 	}
 
 	@Test
 	void testIAuditoriaProcesosDelete() {
-		fail("Not yet implemented");
+		auditoriaProcesosRepository.delete(auditoriaProcesosSearch);
+		Optional<AuditoriaProcesos> auditoriaProcesosFind = auditoriaProcesosRepository
+				.findById(auditoriaProcesosSearch.getId());
+
+		assertThat(auditoriaProcesosFind).isEmpty();
+		log.info("testIAuditoriaProcesosDelete - id: {}", auditoriaProcesosSearch.getId());
 	}
 
 }
