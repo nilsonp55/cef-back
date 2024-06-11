15bd1fbee6c43535947ee37665c73717deea3e7a
Hector Fabio Mercado De La Rosa <prv_hmercadod@ath.com.co>
2024-06-11 11:46:39 -0500

    correcciones sonar - entrega release 3.0

--- src/main/java/com/ath/adminefectivo/service/impl/ParametrosLiquidacionCostosServiceImpl.java
+++ src/main/java/com/ath/adminefectivo/service/impl/ParametrosLiquidacionCostosServiceImpl.java
@@ -1,9 +1,6 @@
 package com.ath.adminefectivo.service.impl;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,11 +8,9 @@ import org.springframework.stereotype.Service;
 import com.ath.adminefectivo.dto.ParametrosLiquidacionCostoDTO;
 import com.ath.adminefectivo.dto.compuestos.EstimadoClasificacionCostosDTO;
 import com.ath.adminefectivo.entities.ParametrosLiquidacionCosto;
-import com.ath.adminefectivo.entities.ValoresLiquidados;
 import com.ath.adminefectivo.repositories.IDetallesLiquidacionCostoRepository;
 import com.ath.adminefectivo.repositories.IParametrosLiquidacionCostosRepository;
 import com.ath.adminefectivo.repositories.IValoresLiquidadosFlatRepository;
-import com.ath.adminefectivo.repositories.IValoresLiquidadosRepository;
 import com.ath.adminefectivo.service.IParametrosLiquidacionCostosService;
 import com.ath.adminefectivo.service.IValoresLiquidadosService;
 
@@ -64,8 +59,8 @@ public class ParametrosLiquidacionCostosServiceImpl implements IParametrosLiquid
 	}
 
 	@Override
-	public ParametrosLiquidacionCosto getParametrosLiquidacionCostosById(Long idLiquidacion) {
-		return parametrosLiquidacionCostosRepository.findById(idLiquidacion).orElse(null); 
+	public Optional<ParametrosLiquidacionCosto> getParametrosLiquidacionCostosById(Long idLiquidacion) {
+		return parametrosLiquidacionCostosRepository.findById(idLiquidacion);
 	}
 
 	@Override
