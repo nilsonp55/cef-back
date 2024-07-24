b0ee978a00495094242894accc1cd0c808c29c8e
Nilson Parra <prv_nparra@ath.com.co>
2024-07-23 19:25:12 -0500

    APCOR-1934

--- build-docker.sh
+++ build-docker.sh
@@ -7,7 +7,7 @@ set ENV_PASS="123546"
 set ENV_SCHEMA="controlefect"
 set ENV_BUCKET="awue1athcef-pt-interfaces"
 set ENV_REGION="us-east-1"
-set JAR_FILE=./target/admin-efectivo-*.jar
+
 
 ### build spring boot app with maven 
 #./mvnw clean verify 
@@ -16,4 +16,4 @@ set JAR_FILE=./target/admin-efectivo-*.jar
 #set ENV_PASS="$(aws secretsmanager get-secret-value --secret-id awue1athcef-pt-rds-secrets --query SecretString --output text --region us-east-1)" 
 
 ### Build docker image
-docker build --build-arg ENV_URL=jdbc:postgresql://db:5432/ctrefc --build-arg ENV_USER=controlefect --build-arg ENV_PASS=123546 --build-arg ENV_SCHEMA=public --build-arg ENV_BUCKET=awue1athcef-pt-interfaces --build-arg ENV_REGION=us-east-1 --build-arg JAR_FILE=target/admin-efectivo-*.jar -t awue1athcef-pt-ecr-fargate:latest .
+docker build --build-arg ENV_URL=jdbc:postgresql://db:5432/ctrefc --build-arg ENV_USER=controlefect --build-arg ENV_PASS=123546 --build-arg ENV_SCHEMA=public --build-arg ENV_BUCKET=awue1athcef-pt-interfaces --build-arg ENV_REGION=us-east-1 -t awue1athcef-pt-ecr-fargate:latest .
