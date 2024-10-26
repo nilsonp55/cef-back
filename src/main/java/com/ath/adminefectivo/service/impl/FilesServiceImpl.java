c802ed402ed756a71efc1970e048d5593fe3ec8d
Nilson <prv_nparra@ath.com.co>
2024-10-25 21:18:57 -0500

    Revert "actualizacion archivo fileserviceimplement.java a version develop de hace 5 meses"
    This reverts commit 2039a6ee93e3f5b8a243b9f1aa5ab13596b671fa.

--- src/main/java/com/ath/adminefectivo/service/impl/FilesServiceImpl.java
+++ src/main/java/com/ath/adminefectivo/service/impl/FilesServiceImpl.java
@@ -166,7 +166,7 @@ public class FilesServiceImpl implements IFilesService {
     if (Boolean.TRUE.equals(s3Bucket)) {
       contenidoCarpeta = s3UtilsV2.getObjectsFromPathS3(url);
     } else {
-      File carpeta = new File(url);
+      File carpeta = new File(TEMPORAL_URL);
       contenidoCarpeta = Arrays.asList(carpeta.list());
     }
 
@@ -305,13 +305,14 @@ public class FilesServiceImpl implements IFilesService {
     String[] arregloNombre = nombreArchivo.split(Constantes.EXPRESION_REGULAR_PUNTO);
     nombreArchivo = arregloNombre[0].concat("-" + postfijo);
     Path destinoPath =
-        FileSystems.getDefault().getPath(urlDestino, nombreArchivo.concat("." + arregloNombre[1]));
-    log.debug("origenPath: {} - arregloNombre:{} - nombreArchivo: {} - destinoPath: {}", origenPath, arregloNombre, nombreArchivo, destinoPath);
+        FileSystems.getDefault().getPath(TEMPORAL_URL+"\\Procesados", nombreArchivo.concat("." + arregloNombre[1]));
+    Path originPath =
+            FileSystems.getDefault().getPath(TEMPORAL_URL, arregloNombre[0].concat("." + arregloNombre[1]));
     try {
       if (Boolean.TRUE.equals(s3Bucket)) {
         s3Util.moverObjeto(origenPath.toString(), destinoPath.toString());
       } else {
-        Files.move(origenPath, destinoPath);
+        Files.move(originPath, destinoPath);
       }
 
     } catch (IOException e) {
@@ -402,4 +403,5 @@ public class FilesServiceImpl implements IFilesService {
       throw new IllegalArgumentException();
     }
   }
+
 }
