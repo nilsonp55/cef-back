8dd012adf3b550cdf55667e2ff699d3cc630779b
Nilson Parra <prv_nparra@ath.com.co>
2024-11-19 08:47:36 -0500

    APCOR-2352

--- src/main/java/com/ath/adminefectivo/repositories/IAuditoriaProcesosRepository.java
+++ src/main/java/com/ath/adminefectivo/repositories/IAuditoriaProcesosRepository.java
@@ -1,6 +1,10 @@
 package com.ath.adminefectivo.repositories;
 
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
 import com.ath.adminefectivo.entities.AuditoriaProcesos;
@@ -10,8 +14,11 @@ import com.ath.adminefectivo.entities.id.AuditoriaProcesosPK;
  * Repository encargado de manejar la logica de la entidad AuditoriaProcesos
  *
  * @author duvan.naranjo
+ * @author prv_nparra
  */
 public interface IAuditoriaProcesosRepository
 		extends JpaRepository<AuditoriaProcesos, AuditoriaProcesosPK>, QuerydslPredicateExecutor<AuditoriaProcesos> {
 
+	@Query("SELECT ap FROM AuditoriaProcesos ap GROUP BY ap.fechaCreacion ORDER BY ap.fechaCreacion")
+	List<Date> AuditoriaProcesosFechasProcesadas();
 }
