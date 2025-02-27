25b2c03679b824fe2e8b4a789d05a622516fb8eb
Nilson Parra <prv_nparra@ath.com.co>
2025-02-27 11:40:47 -0500

    APCOR-2527

--- src/main/java/com/ath/adminefectivo/service/impl/MenuServiceImpl.java
+++ src/main/java/com/ath/adminefectivo/service/impl/MenuServiceImpl.java
@@ -2,16 +2,13 @@ package com.ath.adminefectivo.service.impl;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.ath.adminefectivo.dto.MenuDTO;
 import com.ath.adminefectivo.entities.Menu;
 import com.ath.adminefectivo.repositories.MenuRepository;
 import com.ath.adminefectivo.service.IMenuService;
 import com.querydsl.core.types.Predicate;
-
 import lombok.extern.log4j.Log4j2;
 
 @Service
@@ -33,5 +30,31 @@ public class MenuServiceImpl implements IMenuService {
 		log.debug("List Menu size: ", listMenuDTO.size());
 		return listMenuDTO;
 	}
+
+    @Override
+    public MenuDTO createMenu(MenuDTO menu) {
+      log.debug("createMenu: {}", menu.getNombre());
+      var entity = menuRepository.save(MenuDTO.CONVERTER_ENTITY.apply(menu));
+      log.debug("createMenu Nombre: {} - id: {}", entity.getNombre(), entity.getIdMenu());
+      return MenuDTO.CONVERTER_DTO.apply(entity);
+    }
+  
+    @Override
+    public MenuDTO updateMenu(MenuDTO menu) {
+      log.debug("updateMenu: Id: {}", menu.getIdMenu());
+      menuRepository.findById(menu.getIdMenu()).orElseThrow();
+      var entity = MenuDTO.CONVERTER_ENTITY.apply(menu);
+      var entitySaved = menuRepository.save(entity);
+
+      log.debug("updateMenu: Updated Id: {}", entitySaved.getIdMenu());
+      return MenuDTO.CONVERTER_DTO.apply(entitySaved);
+    }
+  
+    @Override
+    public void deleteMenu(String idMenu) {
+      log.debug("deleteMenu: Id: {}", idMenu);
+      menuRepository.deleteById(idMenu);
+      log.debug("deleteMenu: Deleted Id: {}", idMenu);
+    }
 
 }
