5188ac6fd39b6b48cf845f56b7788fff53ed80c9
Jose-Pabon-GFT <prv_jpabon@ath.com.co>
2025-09-16 15:16:12 -0500

    "APCOR-3040: corregir validación costos lógica de sophos y validación archivos validar certificacion"

--- src/main/java/com/ath/adminefectivo/service/IMotorReglasService.java
+++ src/main/java/com/ath/adminefectivo/service/IMotorReglasService.java
@@ -1,6 +1,10 @@
 package com.ath.adminefectivo.service;
 
+import java.util.Map;
+
+import com.ath.adminefectivo.dto.ListaDetalleDTO;
 import com.ath.adminefectivo.dto.ReglasDetalleArchivoDTO;
+import com.ath.adminefectivo.dto.compuestos.ValidacionArchivoDTO;
 import com.ath.adminefectivo.dto.compuestos.ValidacionMotorDTO;
 
 /**
@@ -10,36 +14,33 @@ import com.ath.adminefectivo.dto.compuestos.ValidacionMotorDTO;
  */
 public interface IMotorReglasService {
 
-    /**
-     * 
-     * @param regla
-     * @param valorCampo
-     * @return boolean
-     * @author rparra
-     */
-    ValidacionMotorDTO evaluarReglaSimple(String regla, String valorCampo);
+	/**
+	 * 
+	 * @param regla
+	 * @param valorCampo
+	 * @return boolean
+	 * @author rparra
+	 */
+	ValidacionMotorDTO evaluarReglaSimple(String regla, String valorCampo, ValidacionArchivoDTO validacionArchivo, int index, Map<String, ListaDetalleDTO> detalleDefinicionMap);
 
-    /**
-     * Metodo encargado de realizar la ejecución de una regla
-     * 
-     * @param reglaVO
-     * @param valorCampo
-     * @return booolean
-     * @author rparra
-     */
-    boolean compilarRegla(ReglasDetalleArchivoDTO reglaVO, String valorCampo);
+	/**
+	 * Metodo encargado de realizar la ejecución de una regla
+	 * 
+	 * @param reglaVO
+	 * @param valorCampo
+	 * @return booolean
+	 * @author rparra
+	 */
+	boolean compilarRegla(ReglasDetalleArchivoDTO reglaVO, String valorCampo, ValidacionArchivoDTO validacionArchivo, int index, Map<String, ListaDetalleDTO> detalleDefinicionMap);
 
-    /**
-     * 
-     * @param regla
-     * @param valorCampo
-     * @return
-     * @return boolean
-     * @author CamiloBenavides
-     */
-    ValidacionMotorDTO evaluarReglaMultiple(String regla, String valorCampo);
-
-    
-
+	/**
+	 * 
+	 * @param regla
+	 * @param valorCampo
+	 * @return
+	 * @return boolean
+	 * @author CamiloBenavides
+	 */
+	ValidacionMotorDTO evaluarReglaMultiple(String regla, String valorCampo, ValidacionArchivoDTO validacionArchivo, int index, Map<String, ListaDetalleDTO> detalleDefinicionMap);
 
 }
